85f284944b652a3cd06380b4c7d1a092dbe3cd68
ponyesteves <ajesteves@gmail.com>
2017-08-15 10:51:35 -0300

    Add structure for more validations in transactions

--- src/transaction.js
+++ src/transaction.js
@@ -17,9 +17,18 @@ export const addItems = (items, transaction = emptyTransaction()) => (
   items.reduce((transaction, item) => addItem(item, transaction), transaction)
 )
 
-export const validateTransaction = (transaction) => (
+export const validateTransaction = (transaction) => {
+  return validations.reduce((prev, curr) => curr(prev), transaction)
+}
+
+const checkAmount = (transaction) => (
   transaction.items.reduce((prev, curr) => prev + curr.amount, 0) === 0 ? transaction : addAmountNotZeroError(transaction)
 )
+
+const validations = [
+  checkAmount
+]
+
 
 const addAmountNotZeroError = (transation) => (
   { ...transation, errors: ['Transaction Amount not zero'] }
