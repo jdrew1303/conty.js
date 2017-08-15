2f56a88c4ec6c80ac3deba47bcf2deb039974ca6
ponyesteves <ajesteves@gmail.com>
2017-08-15 10:33:33 -0300

    createAccount has now a configuration object

--- src/account.js
+++ src/account.js
@@ -1,14 +1,18 @@
 import { Either, Maybe } from 'ramda-fantasy'
 const [Right, Left] = [Either.Right, Either.Left]
-
+const defaultConf = {
+  validTypes: ['activo', 'pasivo']
+}
 // myHelpers
 import { containsRequiredKeys, containsValidValuesForKeys } from './validators'
 
-export const createAccount = (propsObj) => {
-  return Right(propsObj)
-    .chain(containsRequiredKeys(['name', 'type']))
-    .chain(containsValidValuesForKeys([{ keyName: 'type', validValues: ['activo', 'pasivo'] }]))
-    .either((errors) => ({ errors }), (propsObj) => ({ ...propsObj, _id: propsObj.name }))
+export const createAccount = (conf = defaultConf) => {
+  return (propsObj) => {
+    return Right(propsObj)
+      .chain(containsRequiredKeys(['name', 'type']))
+      .chain(containsValidValuesForKeys([{ keyName: 'type', validValues: conf.validTypes }]))
+      .either((errors) => ({ errors }), (propsObj) => ({ ...propsObj, _id: propsObj.name }))
+  }
 }
 
 
--- tests/account.test.js
+++ tests/account.test.js
@@ -2,48 +2,48 @@ import { createAccount } from '../src'
 
 describe('createAccount missing keys errors', () => {
   it('has errors if missing type', () => {
-    expect(createAccount({ name: 'jose' }).errors.missingKeys).toContain('type')
+    expect(createAccount()({ name: 'jose' }).errors.missingKeys).toContain('type')
   })
   it('has errors if type missing name', () => {
-    expect(createAccount({ type: 'organge' }).errors.missingKeys).toContain('name')
+    expect(createAccount()({ type: 'organge' }).errors.missingKeys).toContain('name')
   })
   it('has errors if type missing name or name', () => {
-    expect(createAccount({}).errors.missingKeys).toContain('name')
-    expect(createAccount({}).errors.missingKeys).toContain('type')
+    expect(createAccount()({}).errors.missingKeys).toContain('name')
+    expect(createAccount()({}).errors.missingKeys).toContain('type')
   })
 })
 
 describe('createAccount valid with all required keys', () => {
   //returns undefined
   it('has errors if type missing name or name', () => {
-    expect(createAccount({ name: 'account', type: 'activo' }).errors).toBeFalsy()
+    expect(createAccount()({ name: 'account', type: 'activo' }).errors).toBeFalsy()
   })
 })
 
 describe('createAccount invalid for missing type', () => {
   it('has errors if type has invalid value', () => {
-    expect(createAccount({ name: 'new', type: 'orange' }).errors.notValidValues.join(',')).toContain('orange')
+    expect(createAccount()({ name: 'new', type: 'orange' }).errors.notValidValues.join(',')).toContain('orange')
   })
 })
 
 describe('createAccount valid for type', () => {
   //returns undefined
   it('has no errors if type has valid value', () => {
-    expect(createAccount({ name: 'new', type: 'activo' }).errors).toBeFalsy()
+    expect(createAccount()({ name: 'new', type: 'activo' }).errors).toBeFalsy()
   })
 })
 
 describe('createAccount valid', () => {
   it('returns object with keys name', () => {
-    expect(Object.keys(createAccount({ name: 'new', type: 'activo' }))).toContain('name')
+    expect(Object.keys(createAccount()({ name: 'new', type: 'activo' }))).toContain('name')
   })
   it('returns object with keys type', () => {
-    expect(Object.keys(createAccount({ name: 'new', type: 'activo' }))).toContain('type')
+    expect(Object.keys(createAccount()({ name: 'new', type: 'activo' }))).toContain('type')
   })
   it('returns object with keys _id', () => {
-    expect(Object.keys(createAccount({ name: 'new', type: 'activo' }))).toContain('_id')
+    expect(Object.keys(createAccount()({ name: 'new', type: 'activo' }))).toContain('_id')
   })
   it('returns object with keys _id === name', () => {
-    expect(createAccount({ name: 'new', type: 'activo' })._id).toBe('new')
+    expect(createAccount()({ name: 'new', type: 'activo' })._id).toBe('new')
   })
 })
